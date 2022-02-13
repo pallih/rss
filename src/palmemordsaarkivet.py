27c2ddf7a8a126d7cd2afd5e0bd67e48bf4d5006
pallih <pallih@gogn.in>
2022-02-13 11:54:00 +0000

    palmemordsarkivet

---
+++ src/palmemordsaarkivet.py
@@ -0,0 +1,37 @@
+import requests
+import csv
+from feedgen.feed import FeedGenerator
+import codecs
+
+
+sheet_id = "1O37mhN5bMt5nd-CaO7ue_3KMbip6eVETWKXwfILsf3E"
+sheet_name = "Beställt"
+sheet_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+
+def make_feed(url):
+    fg = FeedGenerator()
+    fg.title("Palmemordsarkivet.se")
+    fg.author({"name": "Páll Hilmarsson", "email": "pallih@gogn.in"})
+    fg.link(href="Palmemordsarkivet.se", rel="alternate")
+    fg.subtitle("Nýlega birt skjöl úr morðrannsókninni á Olof Palme")
+    fg.language("is")
+    r = requests.get(url)
+    lines = codecs.iterdecode(r.iter_lines(), "utf-8")
+    next(lines)
+    reader = csv.DictReader(lines, delimiter=",")
+    for row in reader:
+        if reader.line_num < 51:
+            fe = fg.add_entry()
+            url = row["Länk till kopia"]
+            fe.id(url)
+            fe.title = row["Titel"]
+            fe.link(href=url)
+            fe.description(row["Titel"])
+        else:
+            break
+    fg.rss_file("rss/palmemordsarkivet.rss", pretty=True)
+
+
+if __name__ == "__main__":
+    make_feed(sheet_url)
